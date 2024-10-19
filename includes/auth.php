75643773b0c2be9ff38be9de692016b6d03b0294
Nirmal Widanage <99357825+NirmalWid@users.noreply.github.com>
2024-10-19 15:52:57 +0530

    Delete includes directory

--- includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-function check_login() {
-    session_start();
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: ../public/login.php");
-        exit();
-    }
-}
--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; 2024 Employee Time Tracking System. All rights reserved.</p>
-</footer>
-</body>
-</html>
